351de9213814d62d8920773627bb493728998c37
Brayan Colina <bcolina88@gmail.com>
2022-05-26 23:24:31 -0400

    Modificaciones vista PDF

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-        <title>PDF Audiencia | Bufette H&M Legal.</title>
+        <title>PDF Casos | Bufette Torrez</title>
 
     </head>
     <body>
